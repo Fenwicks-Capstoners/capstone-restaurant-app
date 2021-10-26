41c2dc0403aceee2b172c9e3604b4985b0d51709
OmarPractice <omarnorombaba@gmail.com>
2021-10-26 16:44:58 -0400

    More research

--- app.js
+++ app.js
@@ -1,16 +1,48 @@
-// app.js
-//this runs the server
-//type npm start or npm run start
-const express = require("express");
+var express = require("express"),
+    app = express(),
+    bodyparser = require("body-parser"),
+    mongoose = require("mongoose");
+const mongoAtlasUri =
+"mongodb+srv://Omar:George72@cluster0.cpwsq.mongodb.net/capstone_database?retryWrites=true&w=majority";
 
-//db instance connection
-require("./config/db");
+try {
+    // Connect to the MongoDB cluster
+    mongoose.connect(
+        mongoAtlasUri,
+        { useNewUrlParser: true, useUnifiedTopology: true },
+        () => console.log(" Mongoose is connected")
+    );
 
-const app = express(); //this file is app?
+} catch (e) {
+    console.log("could not connect");
+}
 
-const port = process.env.PORT || 3301;
+app.use(bodyparser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 
-app.listen(port, () => {
-    console.log(    'Server running at http://localhost:${port}');
-
-});
+var schema = new mongoose.Schema({
+    Name : String,
+    lname : String
+    // route : String,
+    // origin : String,
+    // destination : String,
+    // estimatedTimeOfArrival : String,
+    // date : String,
+    // time : String
+})
+var detailsModel = mongoose.model("userCollection", schema);
+app.get("/", function (req, res) {
+    res.render("index",{ details: null })
+})
+app.get("/getdetails", function (req, res) {
+    detailsModel.find({}, function (err, allDetails) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("index", { details: allDetails })
+        }
+    })
+})
+app.listen(3000, "localhost", function () {
+    console.log("server has started");
+})
