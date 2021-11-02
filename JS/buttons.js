5b5eef7f922ae400656ed7e486fb2b51ced0d854
Jerome Glenn <glennje@appstate.edu>
2021-11-02 15:39:30 -0400

    fresh start

--- JS/buttons.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('.button').forEach(function(e) {
-    e.addEventListener('click', function() {
-        this.style.backgroundColor = "red";
-    })
-});
--- README.md
+++
@@ -1,2 +0,0 @@
-# capstone
-Jerome and Omar capstone repo
