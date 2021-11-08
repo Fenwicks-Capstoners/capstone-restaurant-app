e3278c080e0cb42bcd68a2ec03731094393f6c43
OmarPractice <omarnorombaba@gmail.com>
2021-11-07 22:19:32 -0500

    View Employee page kinda coming along

--- routes/employeeRoute.js
+++ routes/employeeRoute.js
@@ -1,6 +1,14 @@
 var express = require('express');
-const {MongoClient} = require("mongodb");
+var MongoClient = require('mongodb').MongoClient
 var router = express.Router();
+var bodyParser = require('body-parser');
+var createError = require('http-errors');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var requireDir = require('require-dir');
+
+router.use(bodyParser.urlencoded({ extended: false }));
 
 const uri = "mongodb+srv://Omar:George72@cluster0.cpwsq.mongodb.net/capstone_database?retryWrites=true&w=majority";
 MongoClient.connect(uri, (err, client) => {
@@ -9,21 +17,28 @@ MongoClient.connect(uri, (err, client) => {
     uc = db.collection('userCollection');
 });
 
-/* GET home page. */
-//gets route index.js
-router.get('/employeeRoute', function(req, res, next) {
-    //below renders view index.ejs
-    res.render('employeeView', { title: 'Express' });
-    next();
+router.get('/employeeRoute',(req , res) =>{
+    db.collection('userCollection').find().toArray(function(err , i){
+        if (err) return console.log(err)
+
+        res.render('employeeView.ejs',{userCollection: i})
+    })
 });
 
-//gets all http://localhost:3000/index3
-router.get('/employeeRoute', (req, res) => {
-    db.collection('userCollection').find().toArray().then(results => {
-        res.send(results);
-        console.log(results);
-    }).catch(error => console.error(error))
-    // ...
-}); //todo: change to 'employeeRoute'
+// /* GET home page. */
+// router.get('/employeeRoute', function(req, res, next) {
+//     //below renders view index.ejs
+//     res.render('employeeView', { title: 'Express' });
+//     next();
+// });
+
+// //gets all http://localhost:3000/index3
+// router.get('/687687687', (req, res) => {
+//     db.collection('userCollection').find().toArray().then(results => {
+//         res.send(results);
+//         console.log(results);
+//     }).catch(error => console.error(error))
+//     // ...
+// }); //todo: change to 'employeeRoute'
 
 module.exports = router;
